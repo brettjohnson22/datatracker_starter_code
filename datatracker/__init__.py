18b13a6884f2e4459febdc90c1403652ea1b6eee
brettjohnson22 <br.onson@outlook.com>
2020-11-09 14:09:07 -0600

    Added second view page, simpilfied app constructor.

--- datatracker/__init__.py
+++ datatracker/__init__.py
@@ -3,18 +3,11 @@ import os
 from flask import Flask
 
 
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
+def create_app():
+    app = Flask(__name__)
     app.config.from_mapping(
         SECRET_KEY='dev',
     )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
 
     # ensure the instance folder exists
     try:
@@ -24,7 +17,7 @@ def create_app(test_config=None):
 
     from . import sample
     app.register_blueprint(sample.bp)
-    app.add_url_rule('/', endpoint='index')
+    # app.add_url_rule('/', endpoint='index')
 
     @app.route('/hello')
     def hello():
--- datatracker/sample.py
+++ datatracker/sample.py
@@ -13,3 +13,24 @@ def index():
     message = "This text is coming from the 'sample.py' module, not the html file!"
     phrase = "Python is cool!"
     return render_template('sample/index.html', message=message, word=phrase)
+
+
+@bp.route('/postform', methods=('GET', 'POST'))
+def other_example():
+    if request.method == 'POST':
+        page_title = request.form['title']
+        error = None
+
+        if not page_title:
+            error = 'You must enter a title'
+
+        if error is not None:
+            flash(error)
+        elif request.form['title'] == "go home":
+            return redirect(url_for('sample.index'))
+        else:
+            return render_template('sample/postform.html', page_title=page_title)
+
+    else:
+        return render_template('sample/postform.html', page_title="PostForm from Module Function")
+
